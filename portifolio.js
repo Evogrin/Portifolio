d7d68053729d53b1e1ce52f0fe50a56beeb6e1fc
unknown <vitormoscarde@gmail.com>
2022-05-31 13:00:10 -0300

    finished structure

--- portifolio.js
+++ portifolio.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function (){
-    header.classList.toggle ("freeze", window.scrollY > 0);
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("freeze", window.scrollY > 0);
 });
+
